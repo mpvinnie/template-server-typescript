1155cbbe5010f50999d95eda5d078df5c95db484
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-07-04 15:11:08 -0400

    create initial config

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  return response.json({ ok: true })
+})
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!')
+})
