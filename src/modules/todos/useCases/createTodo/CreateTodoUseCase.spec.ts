6e00330704e5a03eac60682eab42e723cbcd781b
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-07-09 10:10:00 -0400

    config jest

---
+++ src/modules/todos/useCases/createTodo/CreateTodoUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe('CreateTodo', () => {
+  it('2 + 2 should be 4', () => {
+    const soma = 2 + 2
+    const result = 4
+
+    expect(soma).toBe(result)
+  })
+
+  it('2 + 2 should not be 5', () => {
+    const soma = 2 + 2
+    const result = 5
+
+    expect(soma).not.toBe(result)
+  })
+})
